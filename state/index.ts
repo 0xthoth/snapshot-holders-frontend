8154dd1a51f26c00fc6a7efbcb609da830096ebe
0xThoth <0xthoth@gmail.com>
2022-06-10 16:56:09 +0700

    updated ignore

--- state/index.ts
+++ state/index.ts
@@ -1,13 +1,11 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
 import { createWrapper } from "next-redux-wrapper";
-import counterReducer from "./counter/counterSlice";
 import appReducer from './app/appSlice';
 
 export const makeStore = () =>
   configureStore({
     reducer: {
       app: appReducer,
-      counter: counterReducer,
     },
   });
 
